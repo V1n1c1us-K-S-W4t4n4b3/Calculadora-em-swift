43efda61be9f8237a5e408b43bea884bc3dbb39d
vinicius watanabe <vinikswatanabe@outlook.com>
2024-06-15 01:46:14 -0300

    first commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Usuario on 15/06/24.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
